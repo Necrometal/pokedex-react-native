45c1d68e9eb900781bfae60547c375f0fe930137
Necrometal <gisenael.dev44@gmail.com>
2026-07-07 16:57:28 +0300

    ui: create component rootview

---
+++ components/Layout/RootView.tsx
@@ -0,0 +1,15 @@
+import { ViewProps } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ViewStyle } from "react-native/Libraries/StyleSheet/StyleSheetTypes";
+
+type Props = ViewProps
+
+export function RootView({ style, ...rest }: Props) {
+    return <SafeAreaView style={[rootStyle, style]} {...rest}/>
+}
+
+const rootStyle = {
+  flex: 1,
+  paddingHorizontal: 4,
+  paddingTop: 4
+} satisfies ViewStyle
