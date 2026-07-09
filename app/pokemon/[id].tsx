4b54f49e806467e7c70ab8a48f89de846f6af0cd
Necrometal <gisenael.dev44@gmail.com>
2026-07-09 16:52:53 +0300

    fix: fix error fetching loader

--- app/pokemon/[id].tsx
+++ app/pokemon/[id].tsx
@@ -1,13 +1,14 @@
 import PokemonPageLoader from "@/components/Pokemon/PageLoader";
 import PokemonViewDetails from "@/components/Pokemon/ViewDetails";
-import usePokemonDetail from "@/hooks/screen/usePokemonDetail";
+import usePokemonDetail, { usePokemonSpecies } from "@/hooks/screen/usePokemonDetail";
 import { useLocalSearchParams } from "expo-router";
 
 export default function Pokemon() {
   const params = useLocalSearchParams()
-  const { data, isFetching } = usePokemonDetail(parseInt((params.id as string)!))
-
-  return isFetching 
-    ? <PokemonPageLoader loading={isFetching} />
-    : <PokemonViewDetails  pokemon={data} isFetching={isFetching}/>
+  const { data, isPending } = usePokemonDetail(Number((params.id)!))
+  const { data: species, isPending: isPendingSpecies } = usePokemonSpecies(parseInt((params.id as string)!))
+  
+  return isPending || isPendingSpecies 
+    ? <PokemonPageLoader loading={isPending || isPendingSpecies} />
+    : <PokemonViewDetails  pokemon={data} species={species!} isFetching={isPending || isPendingSpecies}/>
 }
